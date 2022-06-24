5db9016c13a4e0897cbaaf6b7c274d26124b1bf1
jeferson-mendes <jefersonmendes3c@gmail.com>
2022-06-24 20:56:48 -0300

    feat: first commit & books module

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_CONECTION_HOST || 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_CONECTION_USERNAME || 'postgres',
+      password: process.env.DATABASE_CONECTION_PASS || '123',
+      database: 'library',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+    }),
+    BooksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
